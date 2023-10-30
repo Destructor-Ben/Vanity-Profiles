a890f95a872c7d903d629ce057f2da86c1d2659f
Destructor_Ben <ben.honeyfield@gmail.com>
2023-10-31 11:27:27 +1300

    Update main mod class

--- VanityProfiles.cs
+++
@@ -1,6 +0,0 @@
-namespace VanityProfiles;
-internal class VanityProfiles : TerraUtilMod
-{
-    // TODO: Change side to NoSync or Both
-    public static VanityProfiles Instance => ModContent.GetInstance<VanityProfiles>();
-}
---
+++ VanityProfilesMod.cs
@@ -0,0 +1,7 @@
+namespace VanityProfiles;
+
+public class VanityProfilesMod : TerraUtilMod
+{
+    // TODO: Change side to NoSync or Both
+    public static VanityProfilesMod Instance => ModContent.GetInstance<VanityProfilesMod>();
+}
